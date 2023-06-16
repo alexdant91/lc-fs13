2e3462ec229ce83af28651453d07cfaf9ecd3b6d
Simone Sbrilly <simonesbrilli@gmail.com>
2023-06-16 10:17:23 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
